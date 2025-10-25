18ae31d08da7bec41afdaf5abee1c870406a0025
hamzaminhal <hamzaminhal2@gmail.com>
2025-10-25 15:13:08 +0500

    env file created

--- firebase-config.js
+++ firebase-config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDF4pRfUuU_eV3WpDLsivUm6nRQnWfGAsg",
-  authDomain: "react-signup-cb819.firebaseapp.com",
-  projectId: "react-signup-cb819",
-  storageBucket: "react-signup-cb819.firebasestorage.app",
-  messagingSenderId: "114637209277",
-  appId: "1:114637209277:web:9f45351a9df1eb74cf81d4"
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
